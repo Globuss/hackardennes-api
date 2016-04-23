1df3ac50ee97540a579ac806224acce6593e32b4
yohang <yohan@giarel.li>
2016-04-23 14:57:15 +0200

    Updated reset

--- reset.sh
+++ reset.sh
@@ -5,3 +5,4 @@ php bin/console doctrine:schema:create
 #php bin/console hautelook_alice:doctrine:fixtures:load
 
 php bin/console app:ImportItinerairesCommand y
+php bin/console app:ImportChateauPathCommand
