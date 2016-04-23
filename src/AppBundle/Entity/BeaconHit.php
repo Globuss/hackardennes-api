33d92539fab20cefb466ab355cf657c9b2dc5e76
Theo P <theo.pierret1@gmail.com>
2016-04-23 19:45:44 +0200

    Stats 4 ever

---
+++ src/AppBundle/Entity/BeaconHit.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table()
+ *
+ * @author Yohan Giarelli <yohan@un-zero-un.fr>
+ */
+class BeaconHit
+{
+	
+	
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     *
+     * @var int
+     */
+    private $id;
+	
+    
+    /**
+     * @Groups({"path"})
+     * @ORM\Column(type="integer")
+     *
+     * @var integer
+     */
+    private $major;
+
+    /**
+     * @Groups({"path"})
+     * @ORM\Column(type="integer")
+     *
+     * @var integer
+     */
+    private $minor;
+
+	/**
+	* @ORM\Column(type="datetime")
+	*/
+    private $dateHit;
+
+    public function __construct()
+    {
+        $this->dateHit = date("Y-m-d H:i:s");
+    }
+
+	public function getMajor(){
+		return $this->major;
+	}
+	
+	public function getMinor(){
+		return $this->minor;
+	}
+	
+	public function getDateHit(){
+		return $this->dateHit;
+	}
+	
+	public function getId(){
+		return $this->id;
+	}
+	
+	public function setMajor($major){
+		$this->major = $major;
+	}
+	
+	public function setMinor($minor){
+		$this->minor = $minor;
+	}
+
+	public function setDateHit($dateHit){
+		$this->dateHit = $dateHit;
+	}
+}
