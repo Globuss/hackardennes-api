95572708c24eeb19c6147ba0a7a3dfd67f52e3a2
yohang <yohan@giarel.li>
2016-04-23 05:39:36 +0200

    Added some old things that i lost

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Hautelook\AliceBundle\HautelookAliceBundle(),
+            new Dunglas\ApiBundle\DunglasApiBundle(),
             new AppBundle\AppBundle(),
         ];
 
