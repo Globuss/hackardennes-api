20f65ea9c57dbaf2c129aae08807bf3beb007f8d
yohang <yohan@giarel.li>
2016-04-23 15:09:59 +0200

    added point by major minor

--- src/AppBundle/Controller/PointController.php
+++ src/AppBundle/Controller/PointController.php
@@ -4,14 +4,17 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Point;
 use Dunglas\ApiBundle\Controller\ResourceController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @author Yohan Giarelli <yohan@un-zero-un.fr>
  */
 class PointController extends ResourceController
 {
-    public function getByMajorAndMinorAction(Point $point)
+    public function getByMajorMinorAction(Request $request, Point $point)
     {
+        $resource = $this->getResource($request);
 
+        return $this->getSuccessResponse($resource, $point);
     }
 }
