ddf7118fdd9ff033d55faaa17d83f11ffe33a5c4
Loïc Schamber <loic@un-zero-un.fr>
2016-04-23 19:43:34 +0200

    Adding of Service Provider ( Hotel, camping, restaurant ) and and import service + updating the reset.sh

---
+++ src/AppBundle/Command/ImportServiceProviderCommand.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\ServiceProvider;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportServiceProviderCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:ImportServiceProviderCommand')
+            ->setDescription('Import all service provider');
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Starting ImportServicesProviderCommand...');
+
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $output->writeln('Adding Hebergement et restauration - Campings.csv');
+        $file = fopen('csv/Hebergement et restauration - Campings.csv','r');
+        fgetcsv($file);
+
+        while($fields = fgetcsv($file)) {
+            $provider = new ServiceProvider($fields[2]);
+            
+            $provider->setName($fields[1]);
+            $provider->setDescription($fields[37] ?: null);
+            $provider->setLatitude($fields[29]);
+            $provider->setLongitude($fields[30]);
+
+            $em->persist($provider);
+        }
+
+        fclose($file);
+
+
+        $output->writeln('Adding Hebergement et restauration - Restaurants.csv');
+        $file = fopen('csv/Hebergement et restauration - Restaurants.csv','r');
+        fgetcsv($file);
+
+        while($fields = fgetcsv($file)) {
+            $provider = new ServiceProvider($fields[2]);
+
+            $provider->setName($fields[1]);
+            $provider->setDescription($fields[36] ?: null);
+            $provider->setLatitude($fields[28]);
+            $provider->setLongitude($fields[29]);
+
+            $em->persist($provider);
+        }
+
+        fclose($file);
+
+        $output->writeln('Adding Hebergement et restauration - Hotels.csv');
+        $file = fopen('csv/Hebergement et restauration - Hotels.csv','r');
+        fgetcsv($file);
+
+        while($fields = fgetcsv($file)) {
+            $provider = new ServiceProvider($fields[2]);
+
+            $provider->setName($fields[1]);
+            $provider->setDescription($fields[34] ?: null);
+            $provider->setLatitude($fields[27]);
+            $provider->setLongitude($fields[28]);
+
+            $em->persist($provider);
+        }
+
+        fclose($file);
+
+
+
+        $em->flush();
+        $output->writeln('Finish');
+
+    }
+}
---
+++ src/AppBundle/Entity/ServiceProvider.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Entity()
+* @ORM\Table()
+*
+ * @author Loïc Schamber <loic@un-zero-un.fr>
+ */
+class ServiceProvider
+{
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     *
+     * @var string
+     */
+    private $description;
+
+    /**
+     *
+     * @ORM\Column(type="float")
+     *
+     * @var float
+     */
+    private $latitude;
+
+    /**
+     *
+     * @ORM\Column(type="float")
+     *
+     * @var float
+     */
+    private $longitude;
+
+    public function __construct($cat)
+    {
+        $this->category = $cat;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+   /**
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param string $category
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param float $latitude
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param float $longitude
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+    }
+
+
+
+}
